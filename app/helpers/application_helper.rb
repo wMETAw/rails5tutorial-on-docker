9eb86ae2201cffe9ac58a469b37b6091f4b37e64
takashima_ryo <takashima@cloverlab.jp>
2017-02-06 14:43:00 +0900

    flavored-ruby fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def full_title(page_title = "")
+    if page_title.empty?
+        base_title = "Ruby on Rails Tutorial Sample App"
+    else
+        base_title = page_title + " | " + "Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
