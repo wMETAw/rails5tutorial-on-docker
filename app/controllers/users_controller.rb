b0f78555f054679f48a3eb8c945d8ff355ab5a49
takashima_ryo <lio114514@gmail.com>
2017-02-21 18:10:36 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
         @user.send_activation_email
         flash[:info] = "Please check your email to activate your account."
         redirect_to root_url
-    else
+    else 
       render 'new'
     end
   end
@@ -50,15 +51,6 @@ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
 
   # 正しいユーザーかどうか確認
